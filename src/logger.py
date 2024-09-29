d9e89b70b91f0e3f57e6fcc1f4bcb58b31cb3dcd
ilovedata6 <amazonfbaexpert6@gmail.com>
2024-09-29 15:36:43 +0500

    Added data ingestion component, updated .gitignore to include data directory, and removed logging.py file

--- src/logger.py
+++ src/logger.py
@@ -2,31 +2,31 @@ import logging
 import os
 from datetime import datetime
 
-log_root_dir = os.path.join(os.getcwd(), 'logs')
+log_root_dir = os.path.join(os.getcwd(), 'Logs')
 current_date = datetime.now().strftime("%Y-%m-%d")
-hour = datetime.now().strftime("%H")  
+hour = datetime.now().strftime("%H")
 
-# Create the log directory if it doesn't exist
+# Creates Logs Folder if it doesn't exist
 daily_log_path = os.path.join(log_root_dir, current_date)
 os.makedirs(daily_log_path, exist_ok=True)
 
-# Log file name based on the current hour
-log_file = f"{hour}.log"
+# Log file name based on the current hour and date
+log_file = f"{hour}_{current_date}.log"
 log_file_path = os.path.join(daily_log_path, log_file)
 
-# Set up the logging configuration
+# Logging Configuration
 logging.basicConfig(
     filename=log_file_path,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
-# Create a logger
+
 logger = logging.getLogger(__name__)
 
-# Add handlers for each log level if needed (optional, for more control over warning/error logs)
+
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)  # This will print to console for immediate feedback
+console_handler.setLevel(logging.INFO)
 formatter = logging.Formatter("[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 
